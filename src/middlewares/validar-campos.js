f851a46fa763c214c24608a80c843d6825f069d3
alej4ndr0ch <alejandrocuxun@gmail.com>
2025-02-24 11:49:34 -0600

    terminado categories

--- src/middlewares/validar-campos.js
+++ src/middlewares/validar-campos.js
@@ -1,11 +1,15 @@
-import { validationResult } from 'express-validator';
+import { validationResult } from "express-validator";
 
 export const validarCampos = (req, res, next) => {
-
+    
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        return next(errors);
+    if (!errors.isEmpty()) {
+        
+        return res.status(400).json({
+            success: false,
+            errors: errors.array()
+        });
     }
 
     next();
